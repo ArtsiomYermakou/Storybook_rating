58e1b18674e9d49e878eaa9226638a77629e84d9
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-07-08 21:18:06 +0300

    refactor input.stories.tsx

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import {action} from '@storybook/addon-actions';
 
 export default {
@@ -22,6 +22,31 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 
     return <><input /><button onClick={()=>{ setValue("dsad")}}>save</button> - actual value: {value}  </>
 }
+
+export const ControlledInput = () => {
+
+    const [parentValue, setParentValue] = useState("")
+
+    const onChange = (e:ChangeEvent<HTMLInputElement>)=> {setParentValue(e.currentTarget.value)}
+
+    return <input value={parentValue} onChange={onChange} />
+
+}
+export const ControlledCheckbox = () => {
+
+    const [parentValue, setParentValue] = useState(true)
+
+    const onChange = (e:ChangeEvent<HTMLInputElement>)=> {
+        setParentValue(e.currentTarget.checked)
+    }
+
+    return <input type="checkbox" checked={parentValue} onChange={onChange} />
+
+}
+export const ControlledSelect = () => {
+
+}
+
 
 export const СontrolledInputWithFixedValue = () => <input value={"Ermakov Artem"}/>
 
