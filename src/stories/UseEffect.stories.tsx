e738c53ad7457640482e355eaea42679d8032e9b
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-08-16 14:42:03 +0300

    less 21 useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -5,20 +5,22 @@ export default {
 }
 
 export const SimpleExample = () => {
-    const [fake, setFake  ] = useState(1);
+    const [fake, setFake] = useState(1);
     const [counter, setCounter] = useState(1);
 
     console.log("Simple Example");
 
-    useEffect( () => {
+    useEffect(() => {
         console.log("useEffect every render");
         document.title = counter.toString();
     })
-    useEffect( () => {
+
+    useEffect(() => {
         console.log("useEffect only first render(componentDidMount)");
         document.title = counter.toString();
     }, [])
-    useEffect( () => {
+
+    useEffect(() => {
         console.log("useEffect first render and every counter change");
         document.title = counter.toString();
     }, [counter])
@@ -26,8 +28,53 @@ export const SimpleExample = () => {
 
     return <>
         Hello, {counter} {fake}
-        <button onClick={ () => setCounter(counter + 1) }>counter+</button>
-        <button onClick={ () => setFake(fake + 1) }>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+    </>
+}
+
+export const SetTimeOutExample = () => {
+    const [fake, setFake] = useState(1);
+    const [counter, setCounter] = useState(1);
+
+    console.log("SetTimeOutExample");
+
+    useEffect(() => {
+
+        setInterval(() => {
+            console.log("tick" + counter)
+            setCounter(counter + 1);
+        }, 1000000);
+
+    })
+    const Time = () => {
+        let a = new Date();
+
+
+
+        const hours = a.getHours()
+        const minutes = a.getMinutes()
+        const seconds = a.getSeconds()
+
+        setInterval(() => {
+
+        }, 1000);
+
+        return (
+            <>
+                {`Время: ${hours}: ${minutes}: ${seconds} `}
+                </>
+        )
+
+
+    }
+
+
+    return <>
+        Hello, counter: {counter} - fake: {fake}<hr/>
+        <Time/>
+        {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+        {/*<button onClick={() => setFake(fake + 1)}>fake+</button>*/}
     </>
 }
 
