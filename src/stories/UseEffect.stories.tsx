7662b1672077aa674aecb414bdbd80adeac5de64
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-08-16 13:14:33 +0300

    less 20 useEffect

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+
+export default {
+    title: "useEffect.memo demo",
+}
+
+export const SimpleExample = () => {
+    const [fake, setFake  ] = useState(1);
+    const [counter, setCounter] = useState(1);
+
+    console.log("Simple Example");
+
+    useEffect( () => {
+        console.log("useEffect every render");
+        document.title = counter.toString();
+    })
+    useEffect( () => {
+        console.log("useEffect only first render(componentDidMount)");
+        document.title = counter.toString();
+    }, [])
+    useEffect( () => {
+        console.log("useEffect first render and every counter change");
+        document.title = counter.toString();
+    }, [counter])
+
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={ () => setCounter(counter + 1) }>counter+</button>
+        <button onClick={ () => setFake(fake + 1) }>fake+</button>
+    </>
+}
+
+
